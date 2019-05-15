4374ffa5390257788e6a3b164b2d5c2b4fa28069
Conor Kingston <cmkingston21@me.com>
2019-05-15 13:00:41 -0700

    lddtool endpoint includes support for application/json content-type header

--- routes/lddtool.js
+++ routes/lddtool.js
@@ -17,11 +17,37 @@ router.post('/', function(req, res, next) {
     function clearTempDirs() {
         Promise.all(['tmp', 'tar'].map(dir => fse.emptyDir(dir))).then(() => {
             console.log('emptied temporary directories');
-            getIngestFile();
+
+            const contentType = req.headers['content-type'];
+
+            switch (contentType) {
+                case 'multipart/form-data':
+                    startBusboy();
+                    break;
+                case 'application/json':
+                    const FILENAME = req.body.filename;
+                    const XML = req.body.xml;
+                    const INGEST_BASENAME = path.basename(FILENAME, '.xml');
+                    const WORK_DIR = `tmp/${INGEST_BASENAME}`;
+                    fse.mkdirSync(WORK_DIR);
+                    
+                    fse.writeFileSync('test.txt', "heloo worldddz");
+                    
+                    fse.writeFile(path.resolve(WORK_DIR, FILENAME), XML, function() {
+                        console.log('callback!');
+                        // res.send('API under development- good call!');
+                        runLddTool(INGEST_BASENAME, WORK_DIR, FILENAME, sendJson); 
+                    });
+                    
+                    break;
+                default:
+                    console.error(`unexpected content-type: ${contentType}`);
+                    break;
+            };
         });
     };
     
-    function getIngestFile() {
+    function startBusboy() {
         const busboy = new Busboy({
             headers: req.headers
         });
@@ -40,29 +66,39 @@ router.post('/', function(req, res, next) {
         file.pipe(STREAM);
         
         STREAM.on('close', function() {
-            runLddTool(INGEST_BASENAME, WORK_DIR, filename);
+            runLddTool(INGEST_BASENAME, WORK_DIR, filename, sendTar);
         });
     };
     
-    function runLddTool(basename, workdir, filename) {
+    function runLddTool(basename, workdir, filename, cb) {
         const INGEST_FILE_PATH = path.resolve(__dirname, '../tmp', basename, filename);
         const CMD = shell.exec(`cd ${workdir} && lddtool -lp --sync ${basename}.xml > ${basename}.log`);
         
         console.log(`Exit: ${CMD.code}\n`);
         
-        sendZip(basename);
+        makeTar(basename, cb);
     };
     
-    function sendZip(basename) {
+    function makeTar(basename, cb) {
         // Zip up artifacts and send them back to client
         const TAR_NAME = `tar-${basename}.tar.gz`;
         const TAR_DIR = '../tar';
         const CMD = `cd tmp && tar -zcf ${TAR_NAME} ${basename} && mv ${TAR_NAME} ${TAR_DIR}`;
         
-        shell.exec(CMD);
-        
+        cb(shell.exec(CMD));
+    }
+    
+    function sendTar() {
         res.download(path.resolve(__dirname, TAR_DIR, TAR_NAME));
     };
+    
+    function sendJson(process) {
+        res.json({
+            // some object that contains information about
+            // how the client can download the compressed directory
+            code: process.code
+        })
+    }
 });
 
 module.exports = router;
