7683a073f1209c8321657b3b915ec3e87c095f99
Conor Kingston <cmkingston21@me.com>
2019-05-15 12:56:38 -0700

    installed bodyParser

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const PORT = 3002;
 
 const app = express();
 app.use(express.static('src'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 const lddtool = require('./routes/lddtool.js');
 app.use('/lddtool',lddtool);
