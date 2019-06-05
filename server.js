635f2e18c26694e970af8060edf05c140c478bc5
Conor Kingston <cmkingston21@me.com>
2019-06-05 15:59:40 -0700

    base tag is set by environment variable configured in docker

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(bodyParser.json({
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(function(req, res, next) {
+    res.locals.BASE = process.env.BASE;
+    console.log(res.locals);
+    next();
+});
 
 app.set('views', './views');
 app.set('view engine', 'pug');
